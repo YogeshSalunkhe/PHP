560df2017958039b37267997b46e5f671eae58ef
YogeshSalunkhe <yogeshsalunkhe110303@gmail.com>
2023-04-09 19:59:57 +0530

    Echo Statement In php

---
+++ Basic_Programs/01_Echo_Statement.php
@@ -0,0 +1,6 @@
+<?php
+    echo "<h1>Hello"," PHP <br></h1>";             // Two Strings Seprated
+    echo "Programming". " Journy <br>";         // Two String Concated Using (.)
+
+    print '<h2>Hello World</h2>';                  //Use for print only one string 
+?>
