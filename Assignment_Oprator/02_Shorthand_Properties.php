622865a1b8c5e7cf38799d3fe5ec5c4dfbab835b
YogeshSalunkhe <yogeshsalunkhe110303@gmail.com>
2023-04-15 09:00:01 +0530

    ShortHand Properties In PHP

---
+++ Assignment_Oprator/02_Shorthand_Properties.php
@@ -0,0 +1,22 @@
+<?php
+        $num1 = 14;
+        $num2 = 7;
+
+        $num2 += $num1;                         //Addition Using ShortHand
+        echo $num2 . "<br>";
+
+        $num2 -= $num1;                        //Substraction Using ShortHand
+        echo $num2 . "<br>";
+
+        $num2 *= $num1;                       //Multiplication Using ShortHand
+        echo $num2 . "<br>";
+
+        $num2 /= $num1;                       //Division Using ShortHand
+        echo $num2 . "<br>";
+
+        $num2 %= $num1;                       //Modulation Using ShortHand
+        echo $num2 . "<br>";
+
+        $num2 **= $num1;                      //Exponention Using ShortHand
+        echo $num2 ;
+?>
