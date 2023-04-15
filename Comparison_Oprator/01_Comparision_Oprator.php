78168a7e779f78bf905008d2c9f279f61a218e0b
YogeshSalunkhe <yogeshsalunkhe110303@gmail.com>
2023-04-15 09:01:12 +0530

    Comparision Oprators In PHP

---
+++ Comparison_Oprator/01_Comparision_Oprator.php
@@ -0,0 +1,29 @@
+<?php
+
+    $num1 = 10;
+    $num2 = 10;
+
+    echo $num1 == $num2;
+    echo "<br>";
+    echo $num1 === $num2;
+    echo "<br>";
+    echo $num1 != $num2;
+    echo "<br>";
+    echo $num1 <> $num2;
+    echo "<br>";
+    echo $num1 !== $num2;
+    echo "<br>";
+    
+    $num1 = 15;
+    echo $num1 > $num2;
+    echo "<br>";
+    echo $num2 < $num1;
+    echo "<br>";
+    echo $num1 >= $num2;
+    echo "<br>";
+    echo $num2 < $num1;
+    echo "<br>";
+
+    echo $num1 <=> $num2;
+
+?>
