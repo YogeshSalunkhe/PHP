45b5e44305866153fc4067ae2892c249b1ba6dd5
YogeshSalunkhe <yogeshsalunkhe110303@gmail.com>
2023-04-16 21:15:00 +0530

    if else if statement in php

---
+++ Conditional_Control_Statements/03_if_else_if_Statement.php
@@ -0,0 +1,15 @@
+<?php
+
+    $num1 = 10;
+    $num2 = 11;
+
+    if($num1 === $num2){
+        echo $num1 ." And ".$num2 ." Are Equal!!! <br>";
+    }
+    elseif($num1 > $num2){
+        echo $num1 . " Is Maximum Number !!!<br>";
+    }
+    else{
+        echo $num2 ." Is Maximum Number !!!";
+    }
+?>
