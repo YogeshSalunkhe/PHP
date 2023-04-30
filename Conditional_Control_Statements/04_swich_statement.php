4b5b90fe1c4ef4ede3b61e91247cbb09a820591f
YogeshSalunkhe <yogeshsalunkhe110303@gmail.com>
2023-04-30 20:10:43 +0530

    switch statement in php

---
+++ Conditional_Control_Statements/04_swich_statement.php
@@ -0,0 +1,36 @@
+<?php
+    $Weekday = 4;
+
+    switch($Weekday){
+        case 1:
+            echo "Today Is Monday";
+            break;
+
+        case 2:
+            echo "Today Is Tuesday";
+            break;
+
+        case 3:
+            echo "Today Is Wenseday";
+            break;
+
+        case 4:
+            echo "Today Is Thursday";
+            break;
+
+        case 5:
+            echo "Today Is Friday";
+            break;
+
+        case 6:
+            echo "Today Is Saturday";
+            break;
+        
+        case 7:
+            echo "Today Is Sunday";
+            break;
+
+        default:
+            echo "Enter The Valid WeekDay Number !!!";
+    }
+?>
