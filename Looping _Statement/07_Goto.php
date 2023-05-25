80ec5f5a5b39ffc6018ab827194730ec7d5f4776
YogeshSalunkhe <yogeshsalunkhe110303@gmail.com>
2023-05-25 20:35:14 +0530

    Goto Statement In PhP

---
+++ Looping _Statement/07_Goto.php
@@ -0,0 +1,13 @@
+<?php
+    echo "Hello World<br>";
+    echo "Hello World<br>";
+    echo "Hello World<br>";
+    goto Dwn;
+    echo "Hello World<br>";
+    echo "Hello World<br>";
+    Dwn:
+    echo "After Jumping!!!<br>";
+    echo "Hello World<br>";
+    echo "Hello World<br>";
+
+?>
