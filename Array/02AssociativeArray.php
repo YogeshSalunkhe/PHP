0de4c9c1200ede0df9e283289573a214df0f5cdb
YogeshSalunkhe <yogeshsalunkhe110303@gmail.com>
2023-05-29 10:41:10 +0530

    Associative Array In PHP

---
+++ Array/02AssociativeArray.php
@@ -0,0 +1,16 @@
+<?php
+    $a = array(
+        "First"=> 10,
+        "Second"=> "Middle",
+        "Third"=> 30
+    );
+
+    echo "<pre>";
+    print_r($a);
+    echo "<pre>";
+
+    echo "<pre>";
+    var_dump($a);
+    echo "<pre>";
+
+?>
