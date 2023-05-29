3138c32eddb15f12c2495b204aa00c6b919bec81
YogeshSalunkhe <yogeshsalunkhe110303@gmail.com>
2023-05-29 10:40:12 +0530

    Indexed Array In PHP

---
+++ Array/01IndexedArray.php
@@ -0,0 +1,13 @@
+<?php
+    //$Colors = ["red","yellow","Orenge","Green"];     Other Method For Declare Array
+    $Colors = array("red","yellow","Orenge","Green");
+
+   /*   echo $Colors[0]." ";
+        echo $Colors[1]." ";
+        echo $Colors[2]." ";
+        echo $Colors[3]; */
+
+    echo "<pre>";
+    print_r($Colors);
+    echo "<pre>";
+?>
